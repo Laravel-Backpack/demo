9c6b83b78397a45f44f9e61d8d0ef917160186b7
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-05-03 07:55:26 +0000

    Apply fixes from StyleCI

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,7 +14,7 @@ Route::group([
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
-	// CRUD resources and other admin routes
+    // CRUD resources and other admin routes
     CRUD::resource('monster', 'MonsterCrudController');
     CRUD::resource('icon', 'IconCrudController');
     CRUD::resource('product', 'ProductCrudController');
