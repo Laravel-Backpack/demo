a592cf4bcfde59ea8ac877c378baa0383e3c30a4
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-11-23 13:34:21 +0000

    Apply fixes from StyleCI

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,12 +2,12 @@
 
 $factory->define(App\Models\Product::class, function (Faker\Generator $faker) {
     return [
-        'name' => ucfirst($faker->unique()->sentence()),
+        'name'        => ucfirst($faker->unique()->sentence()),
         'description' => $faker->text(50),
-        'details' => $faker->text(800),
+        'details'     => $faker->text(800),
         // 'features',
 
-        'price' => rand(),
+        'price'       => rand(),
         'category_id' => function () {
             if (rand(1, 100) % 50 == 0) {
                 return factory(Backpack\NewsCRUD\app\Models\Category::class)->create()->id;
