c3c7fb68fcf3ab539998f6d5166e60ae702fd910
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-11-26 10:18:35 +0200

    icon required, monster has export buttons

--- app/Http/Controllers/Admin/MonsterCrudController.php
+++ app/Http/Controllers/Admin/MonsterCrudController.php
@@ -497,6 +497,9 @@ class MonsterCrudController extends CrudController
         // ------ DATATABLE EXPORT BUTTONS
         // Show export to PDF, CSV, XLS and Print buttons on the table view.
         // Does not work well with AJAX datatables.
+
+        $this->crud->enableBulkActions();
+        $this->crud->addBulkDeleteButton();
         $this->crud->enableExportButtons();
 
         // ------ FILTERS
--- app/Http/Requests/IconRequest.php
+++ app/Http/Requests/IconRequest.php
@@ -23,7 +23,8 @@ class IconRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name' => 'required|min:5|max:255',
+            'icon' => 'required'
         ];
     }
 
