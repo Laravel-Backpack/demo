42e7ca47187a3f825dbf976424e54fc81748b6a6
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-07-25 12:14:06 +0300

    installed v4-alpha branch and published new assets

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -11,39 +11,61 @@ return [
     |
     */
 
-    // Project name. Shown in the breadcrumbs and a few other places.
-    'project_name' => 'Backpack Demo',
+    // Date & Datetime Format Syntax: https://carbon.nesbot.com/docs/#api-localization
+    'default_date_format'     => 'D MMM YYYY',
+    'default_datetime_format' => 'D MMM YYYY, HH:mm',
 
-    // Menu logos
-    'logo_lg'   => '<b>Back</b>pack',
-    'logo_mini' => '<b>B</b>p',
+    // ----
+    // HEAD
+    // ----
 
-    // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
-
-    // Developer website. Link in footer.
-    'developer_link' => 'http://tabacitu.ro',
-
-    // Show powered by Laravel Backpack in the footer?
-    'show_powered_by' => true,
-
-    // The AdminLTE skin. Affects menu color and primary/secondary colors used throughout the application.
-    'skin' => 'skin-purple',
-    // Options: skin-black, skin-blue, skin-purple, skin-red, skin-yellow, skin-green, skin-blue-light, skin-black-light, skin-purple-light, skin-green-light, skin-red-light, skin-yellow-light
-
-    // Date & Datetime Format Syntax: https://github.com/jenssegers/date#usage
-    // (same as Carbon)
-    'default_date_format'     => 'Do MMMM YYYY',
-    'default_datetime_format' => 'Do MMMM YYYY, HH:mm',
+    // Project name. Shown in the window title.
+    'project_name' => 'Backpack Admin Panel',
 
     // Content of the HTML meta robots tag to prevent indexing and link following
     'meta_robots_content' => 'noindex, nofollow',
 
     // Overlays - CSS files that change the look and feel of the admin panel
     'overlays' => [
-        'vendor/backpack/base/backpack.bold.css',
-        'vendor/backpack/base/backpack.content.is.king.css',
+        'packages/digitallyhappy/backstrap/css/backstrap.css',
     ],
+
+    // ------
+    // HEADER
+    // ------
+
+    // Menu logo. You can replace this with an <img> tag if you have a logo.
+    'project_logo'   => '<b>Back</b>pack',
+
+    // Horizontal navbar classes. Helps make the admin panel look similar to your project's design.
+    'header_class' => 'app-header bg-transparent border-0 navbar position-relative',
+        // Try adding bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan
+        // You might need to add "navbar-dark" too if the background color is a dark one.
+
+    // ----
+    // BODY
+    // ----
+
+    // Body element classes.
+    'body_class' => 'app aside-menu-fixed sidebar-lg-show',
+        // Try sidebar-hidden, sidebar-fixed, sidebar-compact, sidebar-lg-show
+
+    // Sidebar element classes.
+    'sidebar_class' => 'sidebar sidebar-pills sidebar-bg-transparent',
+        // Try removing our sidebar-pills class and adding a background class like bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan
+
+    // ------
+    // FOOTER
+    // ------
+
+    // Developer or company name. Shown in footer.
+    'developer_name' => 'Cristian Tabacitu',
+
+    // Developer website. Link in footer. Type false if you want to hide it.
+    'developer_link' => 'http://tabacitu.ro',
+
+    // Show powered by Laravel Backpack in the footer? true/false
+    'show_powered_by' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +73,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Choose whether new users/admins are allowed to register.
-    | This will show up the Register button in the menu and allow access to the
+    | This will show the Register button on the login page and allow access to the
     | Register functions in AuthController.
     |
     | By default the registration is open only on localhost.
@@ -95,6 +117,7 @@ return [
     'middleware_class' => [
         App\Http\Middleware\CheckIfAdmin::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \Backpack\Base\app\Http\Middleware\UseBackpackAuthGuardInsteadOfDefaultAuthGuard::class,
     ],
 
     // Alias for that middleware
@@ -135,4 +158,19 @@ return [
     // You can rename this disk here. Default: root
     'root_disk_name' => 'root',
 
+    /*
+    |--------------------------------------------------------------------------
+    | License Code
+    |--------------------------------------------------------------------------
+    |
+    | If you, your employer or your client make money by using Backpack, you need
+    | to purchase a license. A license code will be provided after purchase,
+    | which you can put here or in your ENV file in staging & production.
+    |
+    | More info and payment form on:
+    | https://www.backpackforlaravel.com
+    |
+    */
+
+    'license_code' => env('BACKPACK_LICENSE', false),
 ];
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,17 +6,9 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
-Route::get('api/article', 'App\Http\Controllers\Api\ArticleController@index');
-Route::get('api/article-search', 'App\Http\Controllers\Api\ArticleController@search');
-Route::get('api/article/{id}', 'App\Http\Controllers\Api\ArticleController@show');
-
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
-    // CRUD resources and other admin routes
-    CRUD::resource('monster', 'MonsterCrudController');
-    CRUD::resource('icon', 'IconCrudController');
-    CRUD::resource('product', 'ProductCrudController');
 }); // this should be the absolute last line of this file
