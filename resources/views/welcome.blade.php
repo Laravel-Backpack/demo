088082bf395abdcbb832376ddffbf897573aa2ad
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-25 10:44:21 +0300

    welcome page from Laravel 6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,98 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- Styles -->
         <style>
             html, body {
-                height: 100%;
-            }
-
-            body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
                 margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
             }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+            .full-height {
+                height: 100vh;
             }
-
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
             .content {
                 text-align: center;
-                display: inline-block;
             }
-
             .title {
-                font-size: 96px;
+                font-size: 84px;
+                color: #605ca8;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            .m-t-lg {
+                margin-top: 60px;
+            }
+            a:hover {
+                color: #605ca8;
             }
         </style>
     </head>
     <body>
-        <div class="container">
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
             <div class="content">
-                <div class="title">Laravel 5</div>
-                <div>This is an empty Laravel app. Backpack does not provide any front-end functionality.<br><br></div>
-                <div><a href="{{ backpack_url() }}">Login to admin panel</a></div>
+                <div class="title m-b-md">
+                    Backpack
+                </div>
+
+                <div class="links">
+                    <a href="{{ backpack_url() }}">Login</a>
+                    <a target="_blank" href="https://backpackforlaravel.com/docs">Docs</a>
+                    <a target="_blank" href="https://github.com/laravel-backpack/crud">GitHub</a>
+                    <a target="_blank" href="https://backpackforlaravel.com/contact">Contact</a>
+                </div>
+
+                <div class="m-t-lg">
+                    * No front-end pages are provided in this demo. Only the admin panel.
+                </div>
             </div>
         </div>
     </body>
