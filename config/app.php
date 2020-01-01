e5f4ecadeb841247f87e2e55ec6c5e55ab9b1064
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-01-01 08:53:30 +0000

    Apply fixes from StyleCI

--- config/app.php
+++ config/app.php
@@ -215,7 +215,7 @@ return [
         'Form'  => Collective\Html\FormFacade::class,
         'Html'  => Collective\Html\HtmlFacade::class,
 
-       /*
+        /*
         * Other Aliases...
         */
 
