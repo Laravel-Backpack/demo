cd39833823e2c296205f97330b9ac58df303cbf4
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-04-17 15:30:36 +0300

    added electric purple overlay css

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -36,6 +36,7 @@ return [
     // CSS files that are loaded in all pages, using Laravel's asset() helper
     'styles' => [
         'packages/backpack/base/css/bundle.css',
+        'admin/css/backpack_electric_purple_overlay.css',
 
         // Here's what's inside the bundle:
         // 'packages/@digitallyhappy/backstrap/css/style.min.css',
