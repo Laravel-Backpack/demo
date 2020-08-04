ac5abe77684f544e02eec2a8319fe625954ce66c
Pedro Martins <pxpm88@gmail.com>
2020-08-04 19:02:29 +0100

    Update alerts.blade.php
    refs: https://github.com/Laravel-Backpack/CRUD/pull/3079

--- resources/views/vendor/backpack/base/inc/alerts.blade.php
+++ resources/views/vendor/backpack/base/inc/alerts.blade.php
@@ -1,47 +1,68 @@
 {{-- Bootstrap Notifications using Prologue Alerts & PNotify JS --}}
+@php
+  $now = \Carbon\Carbon::now();
+  $refreshTime = \Carbon\Carbon::now()->endOfHour();
+  $diffTime = $now->diffInMinutes($refreshTime);    
+@endphp
+
 <script type="text/javascript">
-  Noty.overrideDefaults({
-    layout   : 'topRight',
-    theme    : 'backstrap',
-    timeout  : 2500, 
-    closeWith: ['click', 'button'],
-  });
 
-  @foreach (\Alert::getMessages() as $type => $messages)
+  // This is intentionaly run after dom loads so this way we can avoid showing duplicate alerts
+  // when the user is beeing redirected by persistent table, that happens before this event triggers.
+  
+  window.addEventListener('DOMContentLoaded', function() {
+      //get the php alerts
+      var $crudAlerts = JSON.parse('{!! json_encode( \Alert::getMessages() , JSON_HEX_APOS) !!}');
+      
+      var $diffTime = parseInt('{{ $diffTime }}');
 
-      @foreach ($messages as $message)
-
-        new Noty({
-          type: "{{ $type }}",
-          text: "{!! str_replace('"', "'", $message) !!}"
-        }).show();
-
-      @endforeach
-  @endforeach
-
-  @if (app('env') != 'local')
-  @php
-    $now = \Carbon\Carbon::now();
-    $refreshTime = \Carbon\Carbon::now()->endOfHour();
-
-    if ($now->diffInMinutes($refreshTime) < 3) {
-      @endphp
+          Noty.overrideDefaults({
+              layout   : 'topRight',
+              theme    : 'backstrap',
+              timeout  : 2500,
+              closeWith: ['click', 'button'],
+          });
+  
+  
+      for (var type in $crudAlerts) {
+          for(var message in $crudAlerts[type]) {
+              new Noty({
+                      type: type,
+                      text: $crudAlerts[type][message]
+                  }).show();
+          }
+      }
+      //if the user was redirected by persistent table we get any alerts to show from localstorage
+      //as they are not in session anymore but we previously stored them.
+      $passedAlerts = JSON.parse(localStorage.getItem('passAlerts'));
+  
+      if(typeof $passedAlerts === 'object' && $passedAlerts !== null) {
+          $passedAlerts.forEach(function($item) {
+                  new Noty({
+                      type: $item.type,
+                      text: $item.text
+                  }).show();
+          });
+      }
+@if (app('env') != 'local')
+      if ($diffTime < 3) {
         new Noty({
           type: "info",
           text: "<strong>Demo Refresh in {{ $now->diffInMinutes($refreshTime) }} Minutes</strong><br>You'll lose all changes.",
           timeout  : 5000, 
         }).show();
-      @php
-    }
-    if ($now->diffInMinutes($refreshTime) > 57) {
-      @endphp
+      }
+
+      if ($diffTime > 57) {
         new Noty({
           type: "info",
           text: "<strong>Demo Refreshed {{ 60-$now->diffInMinutes($refreshTime) }} Minutes Ago</strong><br>All custom entries & files have been deleted.",
           timeout  : 5000, 
         }).show();
-      @php
-    }
-  @endphp
-  @endif
-</script>
+      }
+@endif
+      //clear the localstorage alerts
+      localStorage.removeItem('passAlerts');
+  });
+  
+  </script>
