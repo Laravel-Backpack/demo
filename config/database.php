3fef3dfed3bc19ca36a762a29028699a00aafcfb
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2022-03-01 17:53:31 +0200

    fix database config for mysqldump called by backupmanager

--- config/database.php
+++ config/database.php
@@ -64,9 +64,13 @@ return [
             'prefix'                        => '',
             'strict'                        => false,
             'engine'                        => null,
-            'dump_command_path'             => env('MYSQL_DUMP_PATH', '/Applications/MAMP/Library/bin/'), // only the path, so without 'mysqldump' or 'pg_dump'
-            'dump_command_timeout'          => 60 * 5, // 5 minute timeout
-            'dump_using_single_transaction' => true,
+            'dump'                          => [
+               'dump_binary_path' => env('MYSQL_DUMP_PATH', '/opt/homebrew/bin/'),
+               'use_single_transaction',
+               'timeout' => 60 * 5, // 5 minute timeout
+               // 'exclude_tables' => ['table1', 'table2'],
+               // 'add_extra_option' => '--optionname=optionvalue',
+            ],
         ],
 
         'pgsql' => [
