f24c3c4f494db56ffdc64f802680e9f334b51692
Munjal Mayank <munjaldevelopment@gmail.com>
2022-10-11 12:39:48 +0530

    Applied cosmetic changes & call getFieldsArrayForTimeAndSpaceTab as array error appears
    Applied cosmetic changes & call getFieldsArrayForTimeAndSpaceTab as array error appears in date_range

--- app/Http/Controllers/Admin/ColumnMonsterCrudController.php
+++ app/Http/Controllers/Admin/ColumnMonsterCrudController.php
@@ -5,30 +5,29 @@ namespace App\Http\Controllers\Admin;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
- * Class ColumnMonsterCrudController.
- *
+ * Class ColumnMonsterCrudController
+ * @package App\Http\Controllers\Admin
  * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
  */
 class ColumnMonsterCrudController extends MonsterCrudController
 {
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     *
+     * 
      * @return void
      */
     public function setup()
     {
         CRUD::setModel(\App\Models\Monster::class);
-        CRUD::setRoute(config('backpack.base.route_prefix').'/column-monster');
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/column-monster');
         CRUD::setEntityNameStrings('column monster', 'column monsters');
         $this->crud->set('show.setFromDb', false);
     }
 
     /**
      * Define what happens when the List operation is loaded.
-     *
+     * 
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
-     *
      * @return void
      */
     public function setupListOperation()
@@ -36,7 +35,7 @@ class ColumnMonsterCrudController extends MonsterCrudController
         $this->crud->disableResponsiveTable();
 
         $this->crud->addColumns(static::getFieldsArrayForSimpleTab());
-        $this->crud->addColumns($this::getFieldsArrayForTimeAndSpaceTab());
+        $this->crud->addColumns(static::getFieldsArrayForTimeAndSpaceTab());
         $this->crud->addColumns(static::getFieldsArrayForSelectsTab());
         $this->crud->addColumns(static::getFieldsArrayForRelationshipsTab());
         $this->crud->addColumns(static::getFieldsArrayForUploadsTab());
@@ -46,7 +45,7 @@ class ColumnMonsterCrudController extends MonsterCrudController
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);.
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
          */
     }
 
