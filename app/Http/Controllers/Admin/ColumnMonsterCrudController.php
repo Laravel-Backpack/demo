ea96892e47deaaa299c909eb8a7b2f39021af489
Pedro X <pxpm88@gmail.com>
2022-11-17 19:35:21 +0000

    refactor monster column controller

--- app/Http/Controllers/Admin/ColumnMonsterCrudController.php
+++ app/Http/Controllers/Admin/ColumnMonsterCrudController.php
@@ -36,95 +36,42 @@ class ColumnMonsterCrudController extends MonsterCrudController
         $this->crud->disableResponsiveTable();
 
         $timeSpaceColumns = static::getFieldsArrayForTimeAndSpaceTab();
-        // Changed "time_range" column definition (changed "name" in comma separeated & convert "name" key in column definition)
-        if ($timeSpaceColumns) {
-            foreach ($timeSpaceColumns as $columnKey => $timeSpaceColumn) {
-                if ($timeSpaceColumn['type'] == 'date_range') {
-                    $dateRangeFieldName = $timeSpaceColumn['name'];
-                    $dateRangeColumnName = implode(',', $dateRangeFieldName);
-                    unset($timeSpaceColumns[$columnKey]);
-
-                    // Creating new variable array to over-ride date_range column as that is "unset" above
-                    $timeSpaceColumnDateRange = ['name' => $dateRangeColumnName, 'label' => $timeSpaceColumn['label'], 'type' => $timeSpaceColumn['type']];
-                    $timeSpaceColumns[$columnKey] = $timeSpaceColumnDateRange;
-                }
+       
+        foreach ($timeSpaceColumns as $columnKey => $column) {
+            // transform field array names into comma separated string
+            if(is_array($column['name'])) {
+                $timeSpaceColumns[$columnKey]['name'] = implode(',', $column['name']);
             }
         }
 
-        $relationshipColumns = static::getFieldsArrayForRelationshipsTab();
-        // Removing "custom_html" column definition
-        if ($relationshipColumns) {
-            foreach ($relationshipColumns as $columnKey => &$relationshipColumn) {
-                // unset the `col-` bootstrap size classes as they would break the columns in the table.
-                // we should do this in all columns this is just a draft for relationships
-                if (isset($column['wrapper']['class'])) {
-                    $wrapperClasses = explode(' ', $column['wrapper']['class'] ?? '');
-                    $classes = [];
-                    foreach ($wrapperClasses as $class) {
-                        if (!str_starts_with($class, 'col-')) {
-                            array_push($classes, $class);
-                        }
-                    }
-                    $relationshipColumn[$columnKey]['wrapper']['class'] = implode(' ', $classes);
-                }
-            }
-        }
-
-        $miscellaneousColumns = static::getFieldsArrayForMiscellaneousTab();
-        // Adding extra attributes in "range" column definition
-        if ($miscellaneousColumns) {
-            foreach ($miscellaneousColumns as $columnKey => $miscellaneousColumn) {
-                if ($miscellaneousColumn['type'] == 'range') {
-                    // Creating new variable array to over-ride date_range column as that is "unset" above
-                    $miscColumnRange = [
-                        'name'              => $miscellaneousColumn['name'],
-                        'label'             => $miscellaneousColumn['label'],
-                        'type'              => $miscellaneousColumn['type'],
-                        'progress_class'    => 'bg-success',
-                        'is_striped'        => '1',
-                        'attributes'        => $miscellaneousColumn['attributes'],
-                        'tab'               => $miscellaneousColumn['tab'],
-                        'wrapperAttributes' => $miscellaneousColumn['wrapperAttributes'],
-                    ];
-
-                    $miscellaneousColumns[$columnKey] = $miscColumnRange;
-                } elseif ($miscellaneousColumn['type'] == 'color' || $miscellaneousColumn['type'] == 'color_picker') {
-                    // Creating new variable array to over-ride date_range column as that is "unset" above
-                    // Over-write Show color hex setting by variable
-                    $showColorHex = true;
-
-                    $miscColumnRange = [
-                        'name'              => $miscellaneousColumn['name'],
-                        'label'             => $miscellaneousColumn['label'],
-                        'type'              => $miscellaneousColumn['type'],
-                        'showColorHex'      => isset($miscellaneousColumn['showColorHex']) ? '1' : $showColorHex,
-                        'tab'               => $miscellaneousColumn['tab'],
-                        'wrapperAttributes' => $miscellaneousColumn['wrapperAttributes'],
-                    ];
-
-                    $miscellaneousColumns[$columnKey] = $miscColumnRange;
-                }
-            }
-        }
 
         $this->crud->addColumns(static::getFieldsArrayForSimpleTab());
         $this->crud->addColumns($timeSpaceColumns);
-        $this->crud->addColumns($selectTabColumns = static::getFieldsArrayForSelectsTab());
-        $this->crud->addColumns($relationshipColumns);
+        $this->crud->addColumns(static::getFieldsArrayForSelectsTab());
+        $this->crud->addColumns(static::getFieldsArrayForRelationshipsTab());
         $this->crud->addColumns(static::getFieldsArrayForUploadsTab());
         $this->crud->addColumns(static::getFieldsArrayForWysiwygEditorsTab());
-        $this->crud->addColumns($miscellaneousColumns);
+        $this->crud->addColumns(static::getFieldsArrayForMiscellaneousTab()); 
 
-        // remove all custom_html columns
+        
         foreach ($this->crud->columns() as $columnKey => $column) {
+            // remove all custom_html columns
             if (isset($column['type']) && $column['type'] === 'custom_html') {
                 $this->crud->removeColumn($columnKey);
+                continue;
             }
+            // unset the `col-` bootstrap size classes as they would break the columns in the table.
+            if (isset($column['wrapper']['class'])) {
+                $wrapperClasses = explode(' ', $column['wrapper']['class'] ?? '');
+                $classes = [];
+                foreach ($wrapperClasses as $class) {
+                    if (!str_starts_with($class, 'col-')) {
+                        array_push($classes, $class);
+                    }
+                }
+                $column['wrapper']['class'] = implode(' ', $classes);
+            }
+            $this->crud->modifyColumn($columnKey, $column);
         }
-        /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);.
-         */
     }
 }
