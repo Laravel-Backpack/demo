47be08fe09fb2c3aef2be2db0cc2c2a59d302d77
Pedro X <pxpm88@gmail.com>
2022-12-18 11:07:01 +0000

    fix class load error

--- app/Models/PetShop/Passport.php
+++ app/Models/PetShop/Passport.php
@@ -4,6 +4,7 @@ namespace App\Models\PetShop;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Passport extends Model
 {
