6187db2a910b09604f6dfeeca3adafd1f2600a3b
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-04-05 13:35:05 +0300

    fix layout switched - it uses ui config now too

--- app/Http/Middleware/TablerTheme.php
+++ app/Http/Middleware/TablerTheme.php
@@ -10,7 +10,7 @@ class TablerTheme
 {
     public function handle($request, Closure $next): mixed
     {
-        if (config('backpack.base.view_namespace') === 'backpack.theme-tabler::') {
+        if (config('backpack.ui.view_namespace') === 'backpack.theme-tabler::') {
             // Set layout if exist in session
             if (Session::has('backpack.theme-tabler.layout')) {
                 Config::set('backpack.theme-tabler.layout', Session::get('backpack.theme-tabler.layout'));
