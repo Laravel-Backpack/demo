84b7935870a46a0824a5aa443f23a053ee1d6ee6
Mauro Martinez <maurohmartinez@gmail.com>
2023-04-26 12:53:47 +0300

    Remove top bar from vertical layouts; create shortcut blade

--- config/backpack/ui.php
+++ config/backpack/ui.php
@@ -57,7 +57,7 @@ return [
     'home_link' => '',
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<img src="https://backpackforlaravel.com/presentation/img/backpack/logos/backpack_logo_new_color_big.png" class="project-logo" alt="Backpack for Laravel">',
+    'project_logo'   => '<img src="/assets/img/backpack_logo.png" class="project-logo" alt="Backpack for Laravel">',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
