447764df3da5891f034fa4b4a05976b4732179bd
Mauro Martinez <maurohmartinez@gmail.com>
2023-05-02 15:33:53 +0300

    First commit

--- resources/views/vendor/backpack/theme-tabler/auth/login/inc/form.blade.php
+++ resources/views/vendor/backpack/theme-tabler/auth/login/inc/form.blade.php
@@ -3,7 +3,7 @@
     @csrf
     <div class="mb-3">
         <label class="form-label" for="{{ $username }}">{{ config('backpack.base.authentication_column_name') }}</label>
-        <input autofocus type="email" name="{{ $username }}" value="{{ old($username, 'admin@example.com') }}" id="{{ $username }}" class="form-control {{ $errors->has($username) ? 'is-invalid' : '' }}">
+        <input autofocus tabindex="1" type="email" name="{{ $username }}" value="{{ old($username, 'admin@example.com') }}" id="{{ $username }}" class="form-control {{ $errors->has($username) ? 'is-invalid' : '' }}">
         @if ($errors->has($username))
             <div class="invalid-feedback">{{ $errors->first($username) }}</div>
         @endif
@@ -17,7 +17,7 @@
                 </div>
             @endif
         </label>
-        <input type="password" name="password" id="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" value="{{ old($username) ? '' : 'admin' }}">
+        <input tabindex="2" type="password" name="password" id="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" value="{{ old($username) ? '' : 'admin' }}">
         @if ($errors->has('password'))
             <div class="invalid-feedback">{{ $errors->first('password') }}</div>
         @endif
@@ -29,6 +29,6 @@
         </label>
     </div>
     <div class="form-footer">
-        <button type="submit" class="btn btn-primary w-100">{{ trans('backpack::base.login') }}</button>
+        <button tabindex="3" type="submit" class="btn btn-primary w-100">{{ trans('backpack::base.login') }}</button>
     </div>
 </form>
