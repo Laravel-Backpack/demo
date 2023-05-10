6bff7647260fb9d19466e607121ea50ef10c9ec2
Mauro Martinez <maurohmartinez@gmail.com>
2023-05-10 12:54:50 +0300

    First commit

--- resources/views/vendor/backpack/theme-tabler/auth/login/inc/form.blade.php
+++ resources/views/vendor/backpack/theme-tabler/auth/login/inc/form.blade.php
@@ -11,24 +11,24 @@
     <div class="mb-2">
         <label class="form-label" for="password">
             {{ trans('backpack::base.password') }}
-            @if (backpack_users_have_email())
-                <div class="form-label-description">
-                    <a href="{{ route('backpack.auth.password.reset') }}">{{ trans('backpack::base.forgot_your_password') }}</a>
-                </div>
-            @endif
         </label>
         <input tabindex="2" type="password" name="password" id="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" value="{{ old($username) ? '' : 'admin' }}">
         @if ($errors->has('password'))
             <div class="invalid-feedback">{{ $errors->first('password') }}</div>
         @endif
     </div>
-    <div class="mb-2">
-        <label class="form-check">
-            <input type="checkbox" class="form-check-input">
+    <div class="d-flex justify-content-between align-items-center mb-2">
+        <label class="form-check mb-0">
+            <input tabindex="3" type="checkbox" class="form-check-input">
             <span class="form-check-label">{{ trans('backpack::base.remember_me') }}</span>
         </label>
+        @if (backpack_users_have_email())
+            <div class="form-label-description">
+                <a tabindex="4" href="{{ route('backpack.auth.password.reset') }}">{{ trans('backpack::base.forgot_your_password') }}</a>
+            </div>
+        @endif
     </div>
     <div class="form-footer">
-        <button tabindex="3" type="submit" class="btn btn-primary w-100">{{ trans('backpack::base.login') }}</button>
+        <button tabindex="5" type="submit" class="btn btn-primary w-100">{{ trans('backpack::base.login') }}</button>
     </div>
 </form>
