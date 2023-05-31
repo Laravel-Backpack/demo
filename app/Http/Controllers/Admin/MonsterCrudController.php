28270ec9a44e920edfe496373483289e556f7f14
Pedro X <pxpm88@gmail.com>
2023-05-31 11:36:49 +0100

    add dropzone demo

--- app/Http/Controllers/Admin/MonsterCrudController.php
+++ app/Http/Controllers/Admin/MonsterCrudController.php
@@ -14,6 +14,7 @@ class MonsterCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\FetchOperation;
+    use \Backpack\Pro\Http\Controllers\Operations\DropzoneOperation;
 
     public function setup()
     {
@@ -1447,6 +1448,14 @@ class MonsterCrudController extends CrudController
             // 'disk' => config('backpack.base.root_disk_name'), // in case you need to show images from a different disk
             // 'prefix' => 'uploads/images/profile_pictures/' // in case your db value is only the file name (no path), you can use this to prepend your path to the image src (in HTML), before it's shown to the user;
             'tab' => 'Uploads',
+        ];
+
+        $fields[] = [ 
+            'label'        => 'Dropzone - drag&drop '.backpack_pro_badge(),
+            'name'         => 'dropzone',
+            'type'         => 'dropzone',
+            'tab' => 'Uploads',
+            'withFiles' => true,
         ];
 
         return $fields;
