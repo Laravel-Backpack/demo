f08484ddada820208c2c106bc55dc4345fab884f
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-08-30 13:29:48 +0300

    added email verification on registration

--- app/User.php
+++ app/User.php
@@ -4,12 +4,13 @@ namespace App;
 
 use Alert;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasRoles;
     use CrudTrait;
