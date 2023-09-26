9173cd75d82015e6d4b34c05377004e01a181631
Antonio Almeida <promatik@gmail.com>
2023-09-26 14:25:49 +0100

    Added language switcher

--- resources/views/vendor/backpack/theme-coreuiv4/inc/topbar_right_content.blade.php
+++ resources/views/vendor/backpack/theme-coreuiv4/inc/topbar_right_content.blade.php
@@ -1,8 +1,10 @@
 <li class="nav-item me-2">
-    <button class="btn-link border-0 nav-link px-0 shadow-none" data-bs-toggle="modal" data-bs-target="#modal-layout">
+    <button class="btn-link border-0 nav-link px-0 shadow-none bg-transparent" data-bs-toggle="modal" data-bs-target="#modal-layout" style="height: 1.25rem">
         <i class="la la-palette fs-5 me-1"></i>
     </button>
 </li>
+
+@include('backpack.language-switcher::language-switcher')
 
 @section('before_scripts')
     <div class="modal modal-blur fade pe-0" id="modal-layout" tabindex="-1" style="display: none;" aria-modal="false" role="dialog">
