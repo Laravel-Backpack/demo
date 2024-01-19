cce024486e7b2442f35fe272727d031ea85df1fc
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-01-20 00:50:10 +0700

    //Translated into Vietnamese

---
+++ lang/vendor/backpack/vi/crud.php
@@ -0,0 +1,28 @@
+<?php
+//Translated into Vietnamese by TanNhatCMS
+return [
+    // Confirmation messages and bubbles
+    'delete_confirm' => 'Bạn có chắc chắn muốn xóa mục này không?',
+    'delete_confirmation_title' => 'Đã xóa mục',
+    'delete_confirmation_message' => 'Mục đã được xóa thành công.',
+    'delete_confirmation_not_title' => 'Không xóa',
+    'delete_confirmation_not_message' => "Xóa bản ghi đã bị vô hiệu hóa trong bản demo trực tuyến. Oop.",
+    'delete_confirmation_not_deleted_title' => 'Không xóa',
+    'delete_confirmation_not_deleted_message' => 'Không có gì xảy ra. Mục của bạn an toàn.',
+    
+
+    // Bulk actions
+    'bulk_no_entries_selected_title' => 'Không có mục nào được chọn',
+    'bulk_no_entries_selected_message' => 'Vui lòng chọn một hoặc nhiều mục để thực hiện hành động hàng loạt trên chúng.',
+
+    // Bulk confirmation
+    'bulk_delete_are_you_sure' => 'Bạn có chắc chắn muốn xóa các mục này không?',
+    'bulk_delete_sucess_title' => 'Đã xóa mục',
+    'bulk_delete_sucess_message' => ' mục đã được xóa',
+    'bulk_delete_error_title' => 'Xóa thất bại',
+    'bulk_delete_error_message' => 'Bạn không thể xóa hàng loạt các mục trong Bản demo trực tuyến.',
+
+    // Ajax errors
+    'ajax_error_title' => 'Lỗi',
+    'ajax_error_text' => 'Lỗi khi tải trang. Vui lòng làm mới trang.',
+];
---
+++ lang/vi/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    | Translated into Vietnamese by TanNhatCMS
+    */
+
+    'password' => 'Mật khẩu phải có ít nhất sáu ký tự và phải khớp với xác nhận.',
+    'reset'    => 'Mật khẩu của bạn đã được đặt lại!',
+    'sent'     => 'Chúng tôi đã gửi liên kết đặt lại mật khẩu của bạn qua email!',
+    'token'    => 'Mã thông báo đặt lại mật khẩu này không hợp lệ.',
+    'user'     => "Chúng tôi không thể tìm thấy người dùng với địa chỉ email đó.",
+
+
+];
