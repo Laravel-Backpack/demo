033f90e2f2558d56986befe6f122ca3e9c163df1
pxpm <pxpm88@gmail.com>
2024-09-16 15:15:30 +0100

    wrap details row text

--- resources/views/vendor/backpack/crud/details_row/monster.blade.php
+++ resources/views/vendor/backpack/crud/details_row/monster.blade.php
@@ -1,5 +1,5 @@
 <div class="m-t-10 m-b-10 p-l-10 p-r-10 p-t-10 p-b-10">
-	<div class="row">
+	<div class="row text-wrap">
 		<div class="col-md-12">
 			<small>Use the <span class="label label-default">details_row</span> functionality to show more information about the entry, when that information does not fit inside the table column.</small><br><br>
 			<strong>Text:</strong> {{ $entry->text }} <br>
