0672688fb7dbe40e4da02e8b98f3b5ac515e1cd7
pxpm <pxpm88@gmail.com>
2025-02-07 11:36:44 +0000

    wip

--- config/backup.php
+++ config/backup.php
@@ -122,6 +122,31 @@ return [
         'database_dump_file_extension' => '',
 
         'destination' => [
+            /*
+             * The compression algorithm to be used for creating the zip archive.
+             *
+             * If backing up only database, you may choose gzip compression for db dump and no compression at zip.
+             *
+             * Some common algorithms are listed below:
+             * ZipArchive::CM_STORE (no compression at all; set 0 as compression level)
+             * ZipArchive::CM_DEFAULT
+             * ZipArchive::CM_DEFLATE
+             * ZipArchive::CM_BZIP2
+             * ZipArchive::CM_XZ
+             *
+             * For more check https://www.php.net/manual/zip.constants.php and confirm it's supported by your system.
+             */
+            'compression_method' => ZipArchive::CM_DEFAULT,
+
+            /*
+             * The compression level corresponding to the used algorithm; an integer between 0 and 9.
+             *
+             * Check supported levels for the chosen algorithm, usually 1 means the fastest and weakest compression,
+             * while 9 the slowest and strongest one.
+             *
+             * Setting of 0 for some algorithms may switch to the strongest compression.
+             */
+            'compression_level' => 9,
 
             /*
              * The filename prefix used for the backup zip file.
@@ -167,19 +192,19 @@ return [
     'notifications' => [
 
         'notifications' => [
-            \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class         => [],
-            \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => [],
-            \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class        => [],
-            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class     => [],
-            \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class   => [],
-            \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class    => [],
+            Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class         => [],
+            Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => [],
+            Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class        => [],
+            Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class     => [],
+            Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class   => [],
+            Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class    => [],
         ],
 
         /*
          * Here you can specify the notifiable to which the notifications should be sent. The default
          * notifiable will use the variables specified in this config file.
          */
-        'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
+        'notifiable' => Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
             'to' => 'your@example.com',
@@ -223,8 +248,8 @@ return [
             'name'          => env('APP_NAME', 'laravel-backup'),
             'disks'         => ['local'],
             'health_checks' => [
-                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class          => 1,
-                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
+                Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class          => 1,
+                Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
             ],
         ],
 
@@ -250,7 +275,7 @@ return [
          * No matter how you configure it the default strategy will never
          * delete the newest backup.
          */
-        'strategy' => \Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
+        'strategy' => Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
 
         'default_strategy' => [
 
