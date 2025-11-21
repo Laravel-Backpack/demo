e5c8b3a88841698dce8ca7d9230a1cebde00c9c8
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2025-11-21 15:57:30 +0200

    default to fuzzy bkg

--- config/backpack/theme-tabler.php
+++ config/backpack/theme-tabler.php
@@ -41,11 +41,11 @@ return [
     'styles' => [
         base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/backpack-color-palette.css'),
         base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/glass.css'),
-        base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/fuzzy-background.css'),
         base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/vertical-lines-background.css'),
         base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/pinstripe-background.css'),
         base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/paper-background.css'),
         base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/dotted-background.css'),
+        base_path('vendor/backpack/theme-tabler/resources/assets/css/skins/fuzzy-background.css'),
     ],
 
     /**
