f70076a0c735d4adda9eece71bcd3ee94502a210
pxpm <pxpm88@gmail.com>
2026-03-02 12:31:35 +0000

    wip

--- tests/Feature/Admin/EditableMonsterCrudControllerTest.php
+++ tests/Feature/Admin/EditableMonsterCrudControllerTest.php
@@ -3,19 +3,9 @@
 namespace Tests\Feature\Admin;
 
 use App\Http\Controllers\Admin\EditableMonsterCrudController;
-use App\Models\Monster;
 
-class EditableMonsterCrudControllerTest extends \Tests\Feature\Backpack\DefaultTestBase
+class EditableMonsterCrudControllerTest extends MonsterCrudControllerTest
 {
-    use \Tests\Feature\Backpack\DefaultListTests;
-    use \Tests\Feature\Backpack\DefaultCreateTests;
-    use \Tests\Feature\Backpack\DefaultUpdateTests;
-    use \Tests\Feature\Backpack\DefaultDeleteTests;
-    use \Tests\Feature\Backpack\DefaultShowTests;
-
     public string $controller = EditableMonsterCrudController::class;
-    public string $model = Monster::class;
     public string $route = 'editable-monster';
-    // Pass additional parameters to controller routes. eg. ['owner' => 1]
-    public array $routeParameters = []; 
 }
--- tests/Feature/Admin/PetShop/OwnerPetsCrudControllerTest.php
+++ tests/Feature/Admin/PetShop/OwnerPetsCrudControllerTest.php
@@ -3,23 +3,12 @@
 namespace Tests\Feature\Admin\PetShop;
 
 use App\Http\Controllers\Admin\PetShop\OwnerPetsCrudController;
-use App\Models\OwnerPets;
 
-/**
- * NOTE: This test configuration was generated using naming conventions because
- * the CrudController could not be initialized. Please verify that the model,
- * route, and controller values below are correct before running your tests.
- */
 class OwnerPetsCrudControllerTest extends \Tests\Feature\Backpack\DefaultTestBase
 {
-    use \Tests\Feature\Backpack\DefaultCreateTests;
-    use \Tests\Feature\Backpack\DefaultUpdateTests;
-    use \Tests\Feature\Backpack\DefaultListTests;
-    use \Tests\Feature\Backpack\DefaultShowTests;
-
     public string $controller = OwnerPetsCrudController::class;
-    public string $model = OwnerPets::class;
+    public string $model = \App\Models\PetShop\Pet::class;
     public string $route = 'owner-pets';
     // Pass additional parameters to controller routes. eg. ['owner' => 1]
-    public array $routeParameters = []; 
+    public array $routeParameters = ['owner' => 1]; 
 }
